8c2a1d0332c306615a14891db39dec1b921819d3
Anton Muravev <tsb99x@gmail.com>
2025-03-02 23:00:30 +0300

    Advent of Code initial

---
+++ advent-of-code/2015_day_1/part_i.c
@@ -0,0 +1,105 @@
+/*
+
+--- Day 1: Not Quite Lisp ---
+https://adventofcode.com/2015/day/1
+
+$ gcc -O2 -ansi \
+        -Wall -Wextra -Wpedantic -Werror \
+        -fsanitize=undefined,address \
+        -o /tmp/part_i \
+        part_i.c
+
+$ clang -O2 -ansi \
+        -Weverything -Werror \
+        -fsanitize=undefined,address \
+        -o /tmp/part_i \
+        part_i.c
+
+$ tcc -DTEST -b -run part_i.c
+
+*/
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static int what_floor(FILE *stream)
+{
+        /* int ∈ [-32768..32767] */
+        int floor = 0;
+        int ch = 0;
+
+        while ((ch = fgetc(stream)) != EOF) {
+                switch (ch) {
+                case '(':
+                        floor += 1;
+                        break;
+                case ')':
+                        floor -= 1;
+                        break;
+                default:
+                        break;
+                }
+        }
+
+        if (!feof(stream)) {
+                perror("Failed to fgetc");
+                exit(EXIT_FAILURE);
+        }
+
+        return floor;
+}
+
+#ifndef TEST
+
+int main(int argc, char *argv[])
+{
+        int floor;
+
+        (void)argc;
+        (void)argv;
+
+        floor = what_floor(stdin);
+        (void)printf("%d\n", floor);
+
+        return EXIT_SUCCESS;
+}
+
+#else
+
+static FILE *tmpfile_with(const char *sz)
+{
+        FILE *file = tmpfile();
+
+        if (file == NULL) {
+                perror("Failed to tmpfile");
+                exit(EXIT_FAILURE);
+        }
+        if (fputs(sz, file) < 0) {
+                perror("Failed to fputs");
+                exit(EXIT_FAILURE);
+        }
+        rewind(file);
+        return file;
+}
+
+int main(int argc, char *argv[])
+{
+        (void)argc;
+        (void)argv;
+
+        assert(what_floor(tmpfile_with("(())")) == 0);
+        assert(what_floor(tmpfile_with("()()")) == 0);
+        assert(what_floor(tmpfile_with("(((")) == 3);
+        assert(what_floor(tmpfile_with("(()(()(")) == 3);
+        assert(what_floor(tmpfile_with("))(((((")) == 3);
+        assert(what_floor(tmpfile_with("())")) == -1);
+        assert(what_floor(tmpfile_with("))(")) == -1);
+        assert(what_floor(tmpfile_with(")))")) == -3);
+        assert(what_floor(tmpfile_with(")())())")) == -3);
+        (void)puts("All tests were successful!");
+
+        return EXIT_SUCCESS;
+}
+
+#endif
