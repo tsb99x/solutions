1c8aa781fb5976b268791d2acacdb4dab281a172
Anton Muravev <tsb99x@gmail.com>
2026-01-21 22:44:25 +0300

    Add solution for LeetCode 20

---
+++ leetcode/20-valid-parentheses.py
@@ -0,0 +1,16 @@
+mapping = {
+    ")": "(",
+    "]": "[",
+    "}": "{",
+}
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for c in s:
+            if c not in mapping:
+                stack.append(c)
+            elif not stack or stack.pop() != mapping[c]:
+                return False
+        return not stack
