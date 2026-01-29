7c9b5d6efd954a5f54b6c1924aa85612c8304e70
Anton Muravev <tsb99x@gmail.com>
2026-01-29 14:54:30 +0300

    Add solution for LeetCode 242

---
+++ leetcode/242-valid-anagram.py
@@ -0,0 +1,8 @@
+# URL: https://leetcode.com/problems/valid-anagram/description/
+
+from collections import Counter
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
